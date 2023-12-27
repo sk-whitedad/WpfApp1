14cdd2114cd5fe398052b6d1f6b2627b738f13dd
PS <senkos@mail.ru>
2023-12-27 14:48:02 +0300

    2

--- WpfApp1/MVVC/View/MainWindow.xaml.cs
+++ WpfApp1/MVVC/View/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +14,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+enum Role
+{
+    Server,
+    Client
+}
+
 namespace WpfApp1
 {
     /// <summary>
@@ -20,13 +27,46 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        Role role;
         public MainWindow()
         {
             InitializeComponent();
+            if (!(bool)(cbServer.IsChecked))
+            {
+                lbStatusConnect.Content = "";
+                role = Role.Client;
+            }
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            
+            if((sender as Button).Content == "Join")
+            {
+                //подключение
+            }
+            if ((sender as Button).Content.Equals("Send"))
+            {
+                if (tbMessageText.Text != "")
+                    tbChatText.Text += $"{role.ToString()}: {tbMessageText.Text} \r";
+                tbMessageText.Text = "";
+            }
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            lbStatusConnect.Visibility = Visibility.Hidden;
+            btJoin.Visibility = Visibility.Hidden; 
+            role = Role.Server;
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            lbStatusConnect.Visibility = Visibility.Visible;
+            btJoin.Visibility = Visibility.Visible;
+            lbStatusConnect.Content = "Status: disconnected";
+            role = Role.Client;
         }
     }
 }
