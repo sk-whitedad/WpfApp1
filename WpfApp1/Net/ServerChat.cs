c77f935fe75e619fa0c517dc73ced0422bdef967
Comp <senkos@mail.ru>
2024-01-05 21:56:01 +0300

    14-45

--- WpfApp1/Net/ServerChat.cs
+++ WpfApp1/Net/ServerChat.cs
@@ -13,63 +13,6 @@ namespace WpfApp1.Net
 {
     public class ServerChat
     {
-        IPAddress? localAddr { get; set; }
-        TcpListener? server { get; set; }
-        StatusServer statusServer;
-
-        public ServerChat(string ip, IGetStatusServer statusServer)
-        {
-            localAddr = IPAddress.Parse(ip);
-            StatusServer = statusServer;
-        }
-
-        public async Task StartServer()
-        {
-            server = new TcpListener(localAddr, 8888);
-            try
-            {
-                if (server != null)
-                {
-                    server.Start();    // запускаем сервер
-                    statusServer = StatusServer.Started;
-                    while (true)
-                    {
-                        using var tcpClient = await server.AcceptTcpClientAsync();
-                        StatusServer.status = Status.Started;
-                        var stream = tcpClient.GetStream();
-                        byte[] responseData = new byte[1024];
-                        int bytes = 0; 
-                        var response = new StringBuilder();
-                        do
-                        {
-                            StatusServer.status = Status.Reception;
-                            bytes = await stream.ReadAsync(responseData);
-                            response.Append(Encoding.UTF8.GetString(responseData, 0, bytes));
-                        }
-                        while (bytes > 0);
-                        // выводим отправленные клиентом данные
-                        var res = response;
-                    }
-                }
-            }
-            finally
-            {
-                if(server != null)
-                    server.Stop(); // останавливаем сервер
-            }
-        }
-
-        public void StopServer()
-        {
-            StatusServer.status = Status.Stopped;
-            server.Stop(); // останавливаем сервер
-        }
-
-        public StatusServer GetStatus()
-        {
-            if (server != null)
-                return statusServer;
-            else return StatusServer.Stopped;
-        }
+ 
     }
 }
