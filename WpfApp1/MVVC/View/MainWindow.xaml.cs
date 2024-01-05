520ddffef4e5ab134ae0aec88502f07ec92b5bc8
Comp <senkos@mail.ru>
2024-01-05 21:37:47 +0300

    ййй

--- WpfApp1/MVVC/View/MainWindow.xaml.cs
+++ WpfApp1/MVVC/View/MainWindow.xaml.cs
@@ -14,113 +14,27 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WpfApp1.MVVC.Model;
 
 namespace WpfApp1
 {
     enum Role { Server, Client }
     public enum Status { Started, Stopped, Reception, Error}
 
-    public interface IGetStatusServer
-    {
-        public Status status { get; set; }
-        public string Message { get; set; }
-    }
-
-    /// <summary>
+     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, IGetStatusServer
+    public partial class MainWindow : Window
     {
-        Role role;
-        ServerChat server { get; set; }
-        ClientChat client { get; set; }
-        public Status status { get; set; }
-        public string Message { get; set; }
+ 
 
         public MainWindow()
         {
             InitializeComponent();
-            server = new ServerChat(tbIPaddr.Text, this);
-            client = new ClientChat(tbIPaddr.Text);
-            if (cbServer.IsChecked == false && cbServer.IsChecked == null)
-            {
-                role = Role.Client;
-            }
-        }
+         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private void SendMessage(object sender, RoutedEventArgs e)
         {
-            lbStatusConnect.Content = "";
-            if (((Button)sender as Button).Content.Equals("Connect"))
-            {
-                //подключение клиента
-                await client.Connected();
-                    //bStatusConnect.Content = $"Connected to server:{tbIPaddr.Text}";
-                    btJoin.Content = "Disconnect";
-                    cbServer.IsEnabled = false;
-                    tbIPaddr.IsEnabled = false;
-                    tbName.IsEnabled = false;
-            }
-            else if(((Button)sender as Button).Content.Equals("Disconnect"))
-            {
-                //отключение клиента
-                client.Close();
-                btJoin.Content = "Connect";
-                cbServer.IsEnabled = true;
-                tbIPaddr.IsEnabled = true;
-                tbName.IsEnabled = true;
-            }
 
-            if (((Button)sender as Button).Content.Equals("Start"))
-            {
-                //старт сервера
-                btJoin.Content = "Stop";
-                cbServer.IsEnabled = false;
-                tbIPaddr.IsEnabled = false;
-                if(status == Status.Started)
-                    lbStatusConnect.Content = $"Server started: {tbIPaddr.Text}";
-                await server.StartServer();
-
-            }
-            else if (((Button)sender as Button).Content.Equals("Stop"))
-            {
-                //стоп сервера
-                server.StopServer();
-                btJoin.Content = "Start";
-                cbServer.IsEnabled = true;
-                tbIPaddr.IsEnabled = true;
-                if (status == Status.Stopped)
-                    lbStatusConnect.Content = $"Server stopped: {tbIPaddr.Text}";
-            }
-
-            if (((Button)sender as Button).Content.Equals("Send"))
-            {
-                if (tbMessageText.Text != "")
-                {
-                    tbChatText.Text += $"{role.ToString()}: {tbMessageText.Text} \r";
-                    await client.SendMessage(tbMessageText.Text);
-                }
-                tbMessageText.Text = "";
-            }
         }
-
-        private void checkBox_Checked(object sender, RoutedEventArgs e)
-        {
-            role = Role.Server;
-            btJoin.Content = "Start";
-            tbName.IsEnabled = false;
-            lbStatusConnect.Content = "";
-        }
-
-        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
-        {
-            role = Role.Client;
-            btJoin.Content = "Connect";
-            tbName.IsEnabled = true;
-            lbStatusConnect.Content = "";
-        }
-
-     
     }
 }
